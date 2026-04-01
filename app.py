76b01cf2c6ed01790e7e704603f77ef8944959e9
Md Imran Iqbal <imraniqbal704@gmail.com>
2026-04-01 10:54:43 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Read HTML file
-with open("index.html", "r", encoding="utf-8") as f:
-    html_data = f.read()
-
-# Display HTML
-st.components.v1.html(html_data, height=800, scrolling=True)
